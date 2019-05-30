8e1a070d22cf2a9971abeed72a5eb6ea0a695dfe
Michael Blackley <michael.blackley@rea-group.com>
2019-05-30 16:23:17 +1000

    extract example

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,49 +1,12 @@
-import React, { useState } from 'react';
-import renderer, { Text, Input, Row, COLOR } from '../lib/index';
+import React from 'react';
+import renderer from '../lib/index';
 
-import SimpleText from './simple-text';
-import Counter from './counter';
-import Unicorns from './unicorns';
+import Original from './original';
 
 const App = () => {
-  const [changedVal, setChangedVal] = useState('');
-  const [submittedVal, setSubmittedVal] = useState('');
-  const [focusInput, setFocusInput] = useState(true);
-
   return (
     <React.Fragment>
-      <Row>
-        <Text styles={{ color: COLOR.GREEN }}>Squash</Text>
-        <Text styles={{ color: COLOR.RED }}>All</Text>
-        The
-        <Text>Text</Text>
-      </Row>
-      <SimpleText />
-      <Counter />
-      <Unicorns />
-      <Text>----------------</Text>
-      <Text>{`Submitted value: ${submittedVal}`}</Text>
-      <Text>----------------</Text>
-      <Row>
-        <Text
-          styles={{
-            bold: true,
-            color: COLOR.BLACK,
-            backgroundColor: COLOR.WHITE,
-          }}
-        >
-          Enter some text:
-        </Text>
-        <Input
-          value={changedVal}
-          onChange={setChangedVal}
-          onSubmit={s => {
-            setSubmittedVal(s);
-            setFocusInput(false);
-          }}
-          focus={focusInput}
-        />
-      </Row>
+      <Original />
     </React.Fragment>
   );
 };
---
+++ examples/original.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import renderer, { Text, Input, Row, COLOR } from '../lib/index';
+
+import SimpleText from './simple-text';
+import Counter from './counter';
+import Unicorns from './unicorns';
+
+const Original = () => {
+  const [changedVal, setChangedVal] = useState('');
+  const [submittedVal, setSubmittedVal] = useState('');
+  const [focusInput, setFocusInput] = useState(true);
+
+  return (
+    <React.Fragment>
+      <Row>
+        <Text styles={{ color: COLOR.GREEN }}>Squash</Text>
+        <Text styles={{ color: COLOR.RED }}>All</Text>
+        The
+        <Text>Text</Text>
+      </Row>
+      <SimpleText />
+      <Counter />
+      <Unicorns />
+      <Text>----------------</Text>
+      <Text>{`Submitted value: ${submittedVal}`}</Text>
+      <Text>----------------</Text>
+      <Row>
+        <Text
+          styles={{
+            bold: true,
+            color: COLOR.BLACK,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          Enter some text:
+        </Text>
+        <Input
+          value={changedVal}
+          onChange={setChangedVal}
+          onSubmit={s => {
+            setSubmittedVal(s);
+            setFocusInput(false);
+          }}
+          focus={focusInput}
+        />
+      </Row>
+    </React.Fragment>
+  );
+};
+
+export default Original;
